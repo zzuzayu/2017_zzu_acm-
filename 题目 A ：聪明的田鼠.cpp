5d6759c470900ffbdc5be09574cf12bb9a016dc9
zzuzayu <zhongfahai@gmail.com>
2017-04-16 23:53:57 +0800

    Create 题目 A ：聪明的田鼠.cpp

---
+++ 题目 A ：聪明的田鼠.cpp
@@ -0,0 +1,58 @@
+/*
+Time Limit: 1 Sec
+Memory Limit: 128 MB
+Description
+田鼠MIUMIU来到了一片农田，农田可以看成是一个M*N个方格的矩阵。每个方格里放有一定的粮食。
+MIUMIU看到这里，兴奋不已，它要拿走多多的粮食，以备过冬。但MIUMIU要么往前走（向右） ，
+要么往下走。 但它很聪明，一定会找到一条拿走最多粮食的路径。
+MIUMIU目前在入口位置, 坐标为（1,1），出口位置在坐标（M,N）。 
+请你编程，计算一下当MIUMIU走出农田时,最多能拿走多少粮食。
+Input
+第一行：  M  N  （ 1≤M ,N ≤50 ）
+接下来有M行， 每行有N个整数,分别表示方格中的粮食数  0≤ Aij≤100 (i=1,..M, j=1,…N)
+（所有的数之间有一个空格）
+Output
+一个整数，表示MIUMIU能拿走的最多粮食数。
+Sample Input
+4  3
+5  3  7
+5  3  2
+5  5  5
+6  2  5
+Sample Output
+30
+HINT
+Source
+*/
+#include <bits/stdc++.h>
+
+
+using namespace std;
+const int maxn=105;
+int dp[maxn][maxn];
+int a[maxn][maxn];
+int Dp(int m,int n)
+{
+    for(int i=m-1;i>=0;i--)
+    {
+        for(int j=n-1;j>=0;j--)
+        {
+            dp[i][j]=max(dp[i+1][j],dp[i][j+1])+a[i][j];
+        }
+    }
+    return dp[0][0];
+}
+int main()
+{
+    int m,n;
+    cin>>m>>n;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+            {
+                cin>>a[i][j];
+            }
+    }
+    cout<<Dp(m,n)<<endl;
+    return 0;
+}
